45f7babde5c3c3937006b527dc06f6929b7dfa4a
Roman Abad <romandevop@gmail.com>
2020-10-06 15:29:51 -0400

    changed theme of website to halloween, added on scroll chachange navbar color, changed and fixed svg files for size and color

--- index.js
+++ index.js
@@ -91,3 +91,13 @@ function showContact() {
   }
 
   
+
+
+
+
+
+
+  window.onscroll = () => {
+    const nav = document.querySelector('#nav');
+    if(this.scrollY <= 10) nav.className = ''; else nav.className = 'scroll';
+  };
